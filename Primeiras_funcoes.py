e2958b8760806287f278078e8a4312e524029842
Alan Moura Santana <alan.moura2@hotmail.com>
2024-11-28 19:12:19 -0300

    alteração de retorno e argumentos

---
+++ Primeiras_funcoes.py
@@ -0,0 +1,13 @@
+def exibir_mensagem():
+    print("Ola mundo!")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+def exibir_mensagem_3(nome="Anônimo"):
+    print(f"Seja bem vindo {nome}!")
+
+#exibir_mensagem()
+exibir_mensagem_2(nome="José")
+#exibir_mensagem_3()
+#exibir_mensagem_3(nome="Carlos Costa")
